e307e52e4b8d601cfeed53b2eb9ed7e9a5222028
RianBattleSKL <rbattle@skltd.com>
2025-11-03 13:27:11 -0500

    refactored question2.py

--- question2.py
+++ question2.py
@@ -2,39 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandas import DataFrame
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-def create_facebook_scatter_plot(df):
-    sns.scatterplot(x="Sales", y="Ad_Spend_Facebook", data=df)
-    plt.title("Scatter Plot of Sales vs Facebook Ads")
-    plt.xlabel("Sales")
-    plt.ylabel("Facebook Ads")
+def create_scatter_plot(df: DataFrame, x: str, y: str, title: str, x_label: str, y_label: str):
+    sns.scatterplot(x=x, y=y, data=df)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.show()
-
-def create_instagram_scatter_plot(df):
-    sns.scatterplot(x="Sales", y="Ad_Spend_Instagram", data=df)
-    plt.title("Scatter Plot of Sales vs Instagram Ads")
-    plt.xlabel("Sales")
-    plt.ylabel("Instagram Ads")
-    plt.show()
-
-def create_discount_scatter_plot(df: pd.DataFrame):
-    sns.scatterplot(x="Sales", y="Discount_Rate", data=df)
-    plt.title("Scatter Plot of Sales vs Discount Rates")
-    plt.xlabel("Sales")
-    plt.ylabel("Discount Rates")
-    plt.show()
-
-def create_scatter_plot():
-    pass
 
 if __name__ == "__main__":
     sales_data_file_path = r"data\techgear_sales_data.xlsx"
     df = pd.read_excel(sales_data_file_path)
     df.name = "sales"
 
-    create_facebook_scatter_plot(df)
-    create_instagram_scatter_plot(df)
-    create_discount_scatter_plot(df)
+    create_scatter_plot(df, "Sales", "Ad_Spend_Facebook", "Scatter Plot of Sales vs Facebook Ads", "Sales", "Facebook Ads")
+    create_scatter_plot(df, "Sales", "Ad_Spend_Instagram", "Scatter Plot of Sales vs Instagram Ads", "Sales", "Instagram Ads")
+    create_scatter_plot(df, "Sales", "Discount_Rate", "Scatter Plot of Sales vs Discount Rates", "Sales", "Discount Rates")
